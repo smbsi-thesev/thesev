6569125e6bf9eb99a5bb3158afb4a4c9d5916974
MichaelC <michaelc@anycommerce.com>
2013-07-29 16:05:48 -0700

    Ken Burns Code Cleanup

--- resources/kenburns.js
+++ resources/kenburns.js
@@ -11,7 +11,7 @@
 								function(callback) {
 									window.setTimeout(callback, 1000 / 60);
 								};
-		
+		var progression = options.progression || "oddeven";
         var $canvas = $(this);
 		$canvas.data('kenburns',true);
         var ctx = this[0].getContext('2d');
@@ -22,7 +22,7 @@
 		ctx.canvas.height = height;
         var image_paths = options.images;
         var display_time = options.display_time || 7000;
-        var fade_time = Math.min(display_time / 2, options.fade_time || 1000);
+        var fade_time = Math.min(display_time / 2, typeof options.fade_time !== "undefined" ? options.fade_time : 1000);
         var solid_time = display_time - (fade_time * 2);
         var fade_ratio = fade_time - display_time
         var frames_per_second = options.frames_per_second || 30;
@@ -30,7 +30,13 @@
         var zoom_level = 1 / (options.zoom || 2);
 		var padding_level = options.padding;	
 		var clear_color = options.background_color || '#000000';
-
+		
+		if(options.randomize){
+			for(var i = 0; i<image_paths.length; i++){
+				
+				}
+			}
+		app.u.dump(images);
         var images = [];
         $(image_paths).each(function(i, image_path){
             images.push({path:image_path,
@@ -47,15 +53,8 @@
             return  {x: x1 + (x2 - x1) * i,
                      y: y1 + (y2 - y1) * i}
         }
-
-        function interpolate_rect(r1, r2, i) {
-            // Blend one rect in to another
-            var p1 = interpolate_point(r1[0], r1[1], r2[0], r2[1], i);
-            var p2 = interpolate_point(r1[2], r1[3], r2[2], r2[3], i);
-            return [p1.x, p1.y, p2.x, p2.y];
-        }
 		
-		function myinterpolate_rect(r1, r2, i){
+		function interpolate_rect(r1, r2, i){
 			var p1 = interpolate_point(r1[0],r1[1],r2[0],r2[1],i);
 			var p2 = interpolate_point(	r1[0]+r1[2],
 										r1[1]+ r1[3],
@@ -64,41 +63,8 @@
 										i); 
 			return[p1.x, p1.y, p2.x-p1.x, p2.y-p1.y];
 		}
-
-        function scale_rect(r, scale) {
-            // Scale a rect around its center
-            var w = r[2] - r[0];
-            var h = r[3] - r[1];
-            var cx = (r[2] + r[0]) / 2;
-            var cy = (r[3] + r[1]) / 2;
-            var scalew = w * scale;
-            var scaleh = h * scale;
-            return [cx - scalew/2,
-                    cy - scaleh/2,
-                    cx + scalew/2,
-                    cy + scaleh/2];
-        }
-
-        function fit(src_w, src_h, dst_w, dst_h) {
-            // Finds the best-fit rect so that the destination can be covered
-            
-			var src_a = src_w / src_h;
-            var dst_a = dst_w / dst_h;
-            var w = src_h * dst_a;
-            var h = src_h;
-            if (w > src_w)
-            {
-                var w = src_w;
-                var h = src_w / dst_a;
-            }
-            var x = (src_w - w) / 2;
-            var y = (src_h - h) / 2;
-			//app.u.dump(x+".."+y+".."+w+".."+h);
-            return [x, y, x+w, y+h];
-			
-        }
 		
-		function myfit(src_w, src_h, dst_w, dst_h, padding){
+		function fit(src_w, src_h, dst_w, dst_h, padding){
 			var w, h;
 			if((dst_w/src_w)*src_h <= dst_h){
 				w = dst_w;
@@ -118,7 +84,7 @@
 			
 		}
 		
-		function myscale(rect, scale){
+		function scale(rect, scale){
 			var w = rect[2] * scale;
             var h = rect[3] * scale;
             var cx = rect[0] + rect[2]/2;
@@ -139,15 +105,9 @@
                     var iw = image.width;
                     var ih = image.height;
 
-                    var r1 = fit(iw, ih, width, height);
-                    var r2 = scale_rect(r1, zoom_level);
-					
-					var r3 = myfit(iw,ih,width,height, padding_level);
-					var r4 = myscale(r3, zoom_level);
-					
-					app.u.dump(r3);
-					app.u.dump(r4);
-					
+					var r1 = fit(iw,ih,width,height, padding_level);
+					var r2 = scale(fit(iw,ih,width,height,1), zoom_level);
+
                     var align_x = Math.floor(Math.random() * 3) - 1;
                     var align_y = Math.floor(Math.random() * 3) - 1;
                     align_x /= 2;
@@ -155,31 +115,30 @@
 
                     var x = r2[0];
                     r2[0] += x * align_x;
-                    r2[2] += x * align_x;
 
                     var y = r2[1];
                     r2[1] += y * align_y;
-                    r2[3] += y * align_y;
 					
-					x = r4[0];
-                    r4[0] += x * align_x;
-                    
+					var reverse = false;
 					
-					y = r4[1];
-                    r4[1] += y * align_y;
+					switch(progression){
+					case "random" :
+						reverse = (Math.random()*2 - 1) > 0;
+						break;
+					case "oddeven" :
+					default :
+						reverse = (image_index % 2);
+						break;
+					}
 					
-                    if (image_index % 2) {
-                        image_info.r1 = r1;
-                        image_info.r2 = r2;
-                        image_info.r3 = r3;
-                        image_info.r4 = r4;
-                    }
-                    else {
-                        image_info.r1 = r2;
-                        image_info.r2 = r1;
-                        image_info.r3 = r4;
-                        image_info.r4 = r3;
-                    }
+					if (!reverse) {
+						image_info.r1 = r1;
+						image_info.r2 = r2;
+					}
+					else {
+						image_info.r1 = r2;
+						image_info.r2 = r1;
+					}
 
                     if(load_callback) {
                         load_callback();
@@ -199,25 +158,13 @@
             }
             var image_info = get_image_info(image_index);
             if (image_info.loaded) {
-                //var r = interpolate_rect(image_info.r1, image_info.r2, anim);
-				var r = myinterpolate_rect(image_info.r3, image_info.r4, anim);
-				//app.u.dump(r);
+				var r = interpolate_rect(image_info.r3, image_info.r4, anim);
                 var transparency = Math.min(1, fade);
 
                 if (transparency > 0) {
                     ctx.save();
                     ctx.globalAlpha = Math.min(1, transparency);
-					//app.u.dump(r);
-					//app.u.dump(width);
-					//app.u.dump(height);
-                    //ctx.drawImage(image_info.image, r[0], r[1], r[2] - r[0], r[3] - r[1], 0, 0, width, height);
-                    try{
-						ctx.drawImage(image_info.image, r[0], r[1], r[2], r[3]);
-						}
-					catch(e){
-						app.u.dump(e);
-						app.u.dump(r);
-						}
+					ctx.drawImage(image_info.image, r[0], r[1], r[2], r[3]);
                     ctx.restore();
                 }
             }
